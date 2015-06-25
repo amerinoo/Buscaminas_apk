66712e7fb70acc2521906e14169ded032b60ab36
pestomerdes <albertmerino11@gmail.com>
2015-06-25 21:21:14 +0200

    Afegida funcionalitat al menu de AccesoBDActivity per eliminar tots els registres de la base de dades. S'ha de fer que actualitzi la ListView

--- app/src/main/java/com/example/especials/buscaminas/AccesoBDActivity.java
+++ app/src/main/java/com/example/especials/buscaminas/AccesoBDActivity.java
@@ -2,8 +2,12 @@ package com.example.especials.buscaminas;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class AccesoBDActivity extends Activity implements QueryFrag.PartidasListener {
@@ -13,6 +17,33 @@ public class AccesoBDActivity extends Activity implements QueryFrag.PartidasList
         setContentView(R.layout.activity_acceso_bd);
         QueryFrag queryFrag = (QueryFrag) getFragmentManager().findFragmentById(R.id.fragmentQuery);
         queryFrag.setPartidasListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.accesobd, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_limpiarBD:
+                removeAllGames();
+                return true;
+        }
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    private void removeAllGames() {
+        UsuariosSQLiteHelper usdbh =
+                new UsuariosSQLiteHelper(this, "DBPartidas", null, UsuariosSQLiteHelper.version);
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+
+        int a = db.delete(UsuariosSQLiteHelper.nameTable, null, null);
+        String text = String.format(getString(R.string.partidasBorradas),a);
+        showToast(text);
     }
 
     public void goMainActivityBD(View v){
@@ -43,5 +74,9 @@ public class AccesoBDActivity extends Activity implements QueryFrag.PartidasList
             i.putExtra(DetalleActivity.EXTRA_TEXTO,p.getAllLog());
             startActivity(i);
         }
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
