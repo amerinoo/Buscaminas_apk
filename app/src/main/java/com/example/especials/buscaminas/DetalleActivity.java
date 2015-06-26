00cb65bc9674304bd270d12ca16ad62283ef6470
pestomerdes <albertmerino11@gmail.com>
2015-06-26 02:08:28 +0200

    Donada funcionalitat als botons de activity_detalle-land i activity_detalle Activat a DetalleActivity el boto Home de la action bar per tal de tornar enrere

--- app/src/main/java/com/example/especials/buscaminas/DetalleActivity.java
+++ app/src/main/java/com/example/especials/buscaminas/DetalleActivity.java
@@ -1,9 +1,11 @@
 package com.example.especials.buscaminas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class DetalleActivity extends Activity {
@@ -17,7 +19,23 @@ public class DetalleActivity extends Activity {
         setContentView(R.layout.activity_detalle);
         RegistroFrag registroFrag = (RegistroFrag) getFragmentManager().findFragmentById(R.id.fragmentRegistro);
         registroFrag.mostrarRegistro(getIntent().getStringExtra(EXTRA_TEXTO));
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                goAccesoBD(null);
+                return true;
+        }
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    public void goAccesoBD(View v){
+        Intent in = new Intent(DetalleActivity.this,AccesoBDActivity.class);
+        startActivity(in);
+        finish();
+    }
 
 }
