cc988114448037d7378ab94329abfc3ce6c5f2c7
pestomerdes <albertmerino11@gmail.com>
2015-06-26 12:55:48 +0200

    Implementada PartidaBuilder

--- app/src/main/java/com/example/especials/buscaminas/PartidaBuilder.java
+++ app/src/main/java/com/example/especials/buscaminas/PartidaBuilder.java
@@ -1,7 +1,61 @@
 package com.example.especials.buscaminas;
 
+import java.util.List;
+
 /**
  * Created by pestomerdes on 6/26/15.
  */
-public class PartidaBuilder {
+public abstract class PartidaBuilder {
+    public String allLog;
+    public int numeroCasillasRestantes;
+    public int tiempo;
+    public List<Casilla> casillas;
+    public int numBombes;
+
+    public abstract void build();
+
+    //Getters
+
+    public String getAllLog() {
+        return allLog;
+    }
+
+    public int getNumeroCasillasRestantes() {
+        return numeroCasillasRestantes;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public List<Casilla> getCasillas() {
+        return casillas;
+    }
+
+    public int getNumBombes() {
+        return numBombes;
+    }
+
+
+    //Setters
+
+    public void setAllLog(String allLog) {
+        this.allLog = allLog;
+    }
+
+    public void setNumeroCasillasRestantes(int numeroCasillasRestantes) {
+        this.numeroCasillasRestantes = numeroCasillasRestantes;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public void setCasillas(List<Casilla> casillas) {
+        this.casillas = casillas;
+    }
+
+    public void setNumBombes(int numBombes) {
+        this.numBombes = numBombes;
+    }
 }
