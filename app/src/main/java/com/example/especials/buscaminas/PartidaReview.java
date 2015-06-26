3ca99c903a3d851ebc44a1761f3ae368441785de
pestomerdes <albertmerino11@gmail.com>
2015-06-26 03:40:51 +0200

    Començat a implementar el metode makeReview de PartidaReview. Ara PartidaReview implementa la interficie Review.

--- app/src/main/java/com/example/especials/buscaminas/PartidaReview.java
+++ app/src/main/java/com/example/especials/buscaminas/PartidaReview.java
@@ -3,7 +3,7 @@ package com.example.especials.buscaminas;
 /**
  * Created by pestomerdes on 6/26/15.
  */
-public class PartidaReview {
+public class PartidaReview implements Review {
     private Partida p;
 
     public PartidaReview(Partida p) {
@@ -11,7 +11,16 @@ public class PartidaReview {
     }
 
     public void makeReview(){
-
+        String review = "";
+        review += "nc : " + p.numeroCasillas + ";\n";
+        review += "ncr : " + p.numeroCasillasRestantes + ";\n";
+        review += "t : " + p.tiempo + ";\n";
+        review += "allLog : " + p.allLog + ";\n";
+        for(Casilla casilla : Tablero.getTablero().casillas){
+            int covered = (casilla.isCovered()) ? 0 : 1;
+            review += "casilla : " + casilla.getPosition() + " " + covered + " " + "state" + ";\n";
+        }
+        p.review = review;
     }
 
     public void showReview(){
---
+++ app/src/main/java/com/example/especials/buscaminas/Review.java
@@ -0,0 +1,9 @@
+package com.example.especials.buscaminas;
+
+/**
+ * Created by pestomerdes on 6/26/15.
+ */
+public interface Review {
+    public void makeReview();
+    public void showReview();
+}
