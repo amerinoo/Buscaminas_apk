4eaa03a64ea6c50b9cd95a1732d7a49b6a94e7a4
pestomerdes <albertmerino11@gmail.com>
2015-06-26 15:09:22 +0200

    Implementada ReviewPartidaBuilder

--- app/src/main/java/com/example/especials/buscaminas/ReviewPartidaBuilder.java
+++ app/src/main/java/com/example/especials/buscaminas/ReviewPartidaBuilder.java
@@ -4,4 +4,13 @@ package com.example.especials.buscaminas;
  * Created by pestomerdes on 6/26/15.
  */
 public class ReviewPartidaBuilder extends PartidaBuilder {
+    @Override
+    public void build() {
+        Partida p = Tablero.getTablero().partida;
+        p.numeroCasillas = getCasillas().size();
+        p.tiempo = getTiempo();
+        p.numeroCasillasRestantes = getNumeroCasillasRestantes();
+        p.allLog = getAllLog();
+        p.numBombas = getNumBombes();
+    }
 }
