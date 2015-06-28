14ddc768830f6cd317bc004c5741d7e30af12331
Albert Merino <albertmerino11@gmail.com>
2015-06-28 19:05:28 +0200

    Descomentat mètode build de ReviewPartidaBuilder

--- app/src/main/java/com/example/especials/buscaminas/ReviewPartidaBuilder.java
+++ app/src/main/java/com/example/especials/buscaminas/ReviewPartidaBuilder.java
@@ -6,12 +6,12 @@ package com.example.especials.buscaminas;
 public class ReviewPartidaBuilder extends PartidaBuilder {
     @Override
     public void build() {
-        /*Partida p = Tablero.getTablero().partida;
-        p.numeroCasillas = getCasillas().size();
+        Partida p = Tablero.getTablero().partida;
+        p.numeroCasillas = getNumeroCasillas();
         p.tiempo = getTiempo();
         p.numeroCasillasRestantes = getNumeroCasillasRestantes();
         p.allLog = getAllLog();
         p.numBombas = getNumBombes();
-        Tablero.getTablero().casillas = getCasillas();*/
+        Tablero.getTablero().casillas = getCasillas();
     }
 }
